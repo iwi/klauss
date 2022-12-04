ff0ed5ff04aa23d359a16de5e2705469858d7237
iwi <iwi@tuta.io>
2022-12-04 09:38:14 +0000

    refactor: Create modules
    Signed-off-by: iwi <iwi@tuta.io>

---
+++ preparacio.py
@@ -0,0 +1,45 @@
+# Set up del joc
+
+# Dependencies
+import random
+import numpy as np
+
+# Funcions
+
+# Creació del culpable
+def crea_culpable(nombre_de_sospitosos):
+    """ Crear una llista on tots els elements són falsos menys un
+    S'enten que el culpable és l'element True.
+    """
+    culpable = random.choice(range(nombre_de_sospitosos))
+    respostes_reals = np.array(np.zeros(nombre_de_sospitosos), dtype=bool)
+    respostes_reals[culpable] = True
+    return respostes_reals, culpable.
+
+# Creació de les pistes
+def genera_utilitat_pistes(n_pistes):
+    """ Crear una llista de la dificultat de les pistes.
+    """
+    utilitat_pistes = []
+    for pista in range(n_pistes):
+        dificultats = ['determinant',
+                       'util',
+                       'soroll']
+        dificultat = random.choice(dificultats)
+        utilitat_pistes.append(dificultat)
+
+    return utilitat_pistes
+
+def genera_pista(n_sospitosos, culpable, utilitat_pista, utilitats):
+    """ Crea un array de si una pista apunta o no a cada sospitós.
+    """
+    pista_sospitosos = []
+    for sospitos in range(n_sospitosos):
+        if sospitos == culpable:
+            indicador = True
+        else:
+            prob = random.uniform(0, 1)
+            indicador = prob < utilitats[utilitat_pista]
+        pista_sospitosos.append(indicador)
+
+    return pista_sospitosos
