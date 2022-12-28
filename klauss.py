912b50caf4fa59c13cdb3eb9baefbae9d1e68c69
iwi <iwi@tuta.io>
2022-12-28 18:56:54 +0000

    feat: Afegeix creació de culpable i lloc a klaus.py

--- klauss.py
+++ klauss.py
@@ -8,3 +8,57 @@ import pistes
 import investigador as inv
 import lloc
 import llista_sospitosos as sos
+
+# Determinar el nombre de sospitosos
+n_sospitosos = 5
+
+# Determinar la probabilitat de cada tipus de pista d'apuntar a sospitosos no-culpables.
+utilitats = {'determinant': 0,
+             'util': 0.2,
+             'soroll': 0.4}
+
+# Determinar el nivell d'emmascarament de les pistes (emmascaració o emmascarat?)
+# Considerar fer-lo variable? e.g. podem fer que els valors segueixin una distribució.
+# !!passar-ho a enters per a no perdre precisió
+distribucio_nivell_emmascaracio = {'veritat': 0.7,
+                                   'mentida': 0.2}
+                                   #,'inconcloent': 1 - veritat - mentida}
+
+
+
+
+# Creem una llista de sospitosos i decidim qui és el culpable
+sospitosos = ["pinguí", "mona", "foca", "esquirol", "guineu"]
+culpable = "foca"
+
+llista_sospitosos = sos.LlistaSospitosos(sospitosos, culpable)
+
+print(llista_sospitosos)
+
+# Creem un lloc
+nom_lloc = "menjador"
+tipus_lloc = "habitacio casa"
+posicio = [2, 2]
+ll1 = lloc.Lloc(nom_lloc, tipus_lloc, posicio)
+
+print(ll1)
+
+
+# Afegir-hi algunes pistes
+ll1.afegeix_pista(nom_pista = "empremptes",
+                  utilitat_pista = "util",
+                  n_sospitosos_pista = n_sospitosos,
+                  culpable_pista = culpable,
+                  utilitats_pista = utilitats)
+
+ll1.afegeix_pista(nom_pista = "dna",
+                  utilitat_pista = "determinant",
+                  n_sospitosos_pista = n_sospitosos,
+                  culpable_pista = culpable,
+                  utilitats_pista = utilitats)
+
+ll1.afegeix_pista(nom_pista = "cabell",
+                  utilitat_pista = "soroll",
+                  n_sospitosos_pista = n_sospitosos,
+                  culpable_pista = culpable,
+                  utilitats_pista = utilitats)
--- lloc.py
+++ lloc.py
@@ -7,14 +7,27 @@ class Lloc():
     Cada lloc tindrà un número de pistes
     """
 
-    def __init__(self, nom, tipus, posicio):
+    def __init__(self, nom, tipus, coordenades):
         """ Definició del lloc
         """
         self.id = uuid.uuid1()
         self.nom = nom
         self.tipus = tipus
-        self.posicio = posicio
+        self.coordenades = coordenades
         self.pistes = []
+
+    def __str__(self):
+        """Mostra'n els detalls
+        Fent ús del mètode màgic __str__ fem que print() funcioni per a imprimir objectes de tipus pista.
+
+         https://www.pythontutorial.net/python-oop/python-__str__/
+         https://openbookproject.net/thinkcs/python/english3e/classes_and_objects_I.html
+        """
+        return "Id: {} \n".format(self.id) +\
+               "Nom: {} \n".format(self.nom) +\
+               "Tipus: {} \n".format(self.tipus) +\
+               "Coordenades: {} \n".format(self.coordenades) +\
+               "Pistes: {} \n".format(self.pistes)
 
     def afegeix_pista(self,
                       nom_pista,
